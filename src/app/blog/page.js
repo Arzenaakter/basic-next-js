096230ab14d45a44f53040d76e5d12e3e4d8df01
Arzena Akter <arzenaakter@gmail.com>
2023-07-30 15:13:05 +0600

    fetch data within a blogPage component

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,37 +1,42 @@
 import Link from "next/link";
 
-const BlogsPage = () => {
-  const blogs = [
-    {
-      id: 1,
-      year: 2014,
-      title: "title 1",
-    },
-    {
-      id: 2,
-      year: 2016,
-      title: "title 2",
-    },
-    {
-      id: 3,
-      year: 2018,
-      title: "title 3",
-    },
-    {
-      id: 4,
-      year: 2020,
-      title: "title 4",
-    },
-  ];
+const BlogsPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const blogs = await res.json();
+  // const blogs = [
+  //   {
+  //     id: 1,
+  //     year: 2014,
+  //     title: "title 1",
+  //   },
+  //   {
+  //     id: 2,
+  //     year: 2016,
+  //     title: "title 2",
+  //   },
+  //   {
+  //     id: 3,
+  //     year: 2018,
+  //     title: "title 3",
+  //   },
+  //   {
+  //     id: 4,
+  //     year: 2020,
+  //     title: "title 4",
+  //   },
+  // ];
   return (
     <div className="container mx-auto">
-      {blogs.map(({ id, year, title }) => (
-        <li key={id} className="border border-blue-900 my-2">
-          <Link
-            href={{ pathname: `/blog/${year}/${id}`, query: { title: title } }}>
-            {title}
+      {blogs.map(({ id, body, title }) => (
+        <div key={id} className="border border-blue-900 my-2 p-2">
+          <h2 className="text-2xl">
+            {id}. {title}
+          </h2>
+          <p>{body}</p>
+          <Link href={`/blog/${id}`} className="inline-block mt-5">
+            <button className="bg-blue-500 p-2">Details</button>
           </Link>
-        </li>
+        </div>
       ))}
     </div>
   );
